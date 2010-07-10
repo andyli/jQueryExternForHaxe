b6635cbb25ba1624576554ef982534df2597ba15
Andy <andy@onthewings.net>
2010-07-11 02:25:15 +0800

    added Flashembed and History

---
+++ jQueryPlugins/jQueryTools/History.hx
@@ -0,0 +1,14 @@
+package jQueryPlugins.jQueryTools;
+
+import JQuery;
+
+/**
+	History
+	http://flowplayer.org/tools/toolbox/history.html
+**/
+
+extern class History {
+	inline static public function history(jQ:JQuery, _callback:JQueryEvent->String->Void):Void untyped {
+		jQ.history(_callback);
+	}
+}
