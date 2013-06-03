fdc636e79f1e7c5a0e91c65d2ec803c14854d372
Andy Li <andy@onthewings.net>
2013-06-03 17:34:03 +0800

    Place Either at root to workaround https://github.com/HaxeFoundation/haxe/issues/1885

---
+++ Either.hx
@@ -0,0 +1,4 @@
+//It is currently placed at root due to https://github.com/HaxeFoundation/haxe/issues/1885
+//package jQuery.haxe;
+
+@:native("jQuery_haxe_Either") abstract Either<T1, T2>(Dynamic) from T1 from T2 to T1 to T2 {}
--- jQuery/haxe/Either.hx
+++ jQuery/haxe/Either.hx
@@ -1,3 +1,3 @@
 package jQuery.haxe;
 
-abstract Either<T1, T2>(Dynamic) from T1 from T2 to T1 to T2 {}
+typedef Either<T1,T2> = std.Either<T1,T2>;
